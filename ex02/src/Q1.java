344518fde86b60caf5e78557e51a5597fbc43af3
njiix47 <nddkw@dgsw.hs.kr>
2026-07-20 18:52:24 +0900

    1일차 (1~3차시)

---
+++ ex02/src/Q1.java
@@ -0,0 +1,11 @@
+public class Q1 {
+    static void main(String[] args) {
+           for (int i=0; i<3; i++) {
+               System.out.println();
+
+               for (int j=0; j<5; j++) {
+                   System.out.print("*");
+               }
+           }
+    }
+}
