39a6c8a803dcce38e0b8aa38cadc306f86593b14
njiix47 <nddkw@dgsw.hs.kr>
2026-07-21 10:06:33 +0900

    create gitignore file and add *.pdf

--- ex03/src/Main.java
+++ ex03/src/Main.java
@@ -1,3 +1,6 @@
 void main() {
+    ex01 ex01 = new ex01();
 
+//    ex01.doA();
+    ex01.doB();
 }
--- ex03/src/ex01.java
+++ ex03/src/ex01.java
@@ -1,2 +1,25 @@
 public class ex01 {
+
+    public void doA() {
+
+        for (int i = 2; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(i + "*" + j + "=" + j * i + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    public void doB() {
+        int num = 10;
+        switch (num) {
+            case 10 :
+                System.out.println("10입니다");
+                break;
+            default :
+                System.out.println("기본");
+        }
+    }
+
+
 }
